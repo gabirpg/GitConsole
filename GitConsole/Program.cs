9efe03c69094d00f5b5307f6749baad4e522a627
gabi rpg <gabi_rpg@yahoo.com>
2018-09-22 23:15:39 +0300

    local testing - 1.0

--- GitConsole/Program.cs
+++ GitConsole/Program.cs
@@ -6,18 +6,7 @@ namespace GitConsole
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello Git!");
-            Console.WriteLine("Hello Git modified1!");
-            Console.WriteLine("Hello Git modified2!");
-            Console.WriteLine("Diff between stages and commited");
-
-            Console.WriteLine("Commit for a new annotated tag!");
-
-            Console.WriteLine("Commit for a new lightway tag!");
-
-            Console.WriteLine("Commit to remove some unused namespaces on branch testing!");
-
-            Console.WriteLine("Another commit on branch testing!");
+            Console.WriteLine("origin testing 1.0");
 
             Console.ReadLine();
         }
